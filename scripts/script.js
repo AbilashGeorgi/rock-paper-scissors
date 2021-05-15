08190c6444c8ecebfcf5ba8530eabd0b9919d867
Abilash Georgi <abilashchacko@gmail.com>
2021-05-15 21:56:25 +0530

    computerPlay function

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
 console.log("Hello World");
+
+const rock = 'rock';
+const paper = 'paper';
+const scissors = 'scissors';
+
+function computerPlay() {
+    let choice = Math.floor(Math.random() * 3);
+    switch (choice) {
+        case 0:
+            return rock;
+        case 1:
+            return paper;
+        case 2:
+            return scissors;
+        default:
+            //should not happen ( so distribution should be more or less even)
+            return rock;
+    }
+    return rock;
+}
